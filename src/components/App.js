b472b8eaaae5c7471d2cc6e6df787449de981d10
JuliannaRkg544 <juliana.rkg544@gmail.com>
2022-07-07 17:09:55 -0300

    signup-products route created to add products

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,34 @@
-import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import UserContext from '../shared/userContext';
-import Home from './Home';
-import Login from './Login';
-import SignUp from './SignUp';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserContext from "../shared/userContext";
+import Home from "./Home";
+import Login from "./Login";
+import Products from "./Products";
+import SignUp from "./SignUp";
 
 function App() {
+  const [token, setToken] = useState("");
+  const [username, setUsername] = useState("");
 
-    const [token, setToken] = useState('');
-    const [username, setUsername] = useState('');
-
-    return (
-        <UserContext.Provider value={{
-            token, setToken, username, setUsername
-        }}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/sign-up' element={<SignUp />} />
-                </Routes>
-            </BrowserRouter>
-        </UserContext.Provider>
-    );
+  return (
+    <UserContext.Provider
+      value={{
+        token,
+        setToken,
+        username,
+        setUsername,
+      }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/signup-products" element={<Products />} />
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
